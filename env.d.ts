dd00cf0e0d82b755c1f70681ab6e57929de7ea48
hyungju-lee <beegizee1220@gmail.com>
2023-03-18 22:00:29 +0900

    3. 환경변수 및 웹팩 데브서버의 프록시 서버 설정

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_KAKAO_SEARCH_API_PROXY: string;
+    readonly VITE_KAKAO_SEARCH_APP_BASE_URL: string;
+    readonly VITE_KAKAO_REST_API_KEY: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({command, mode}) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: {
+        [String(process.env.VITE_KAKAO_SEARCH_APP_BASE_URL)]: {
+          target: process.env.VITE_KAKAO_SEARCH_API_PROXY,
+          changeOrigin: true,
+        }
+      }
     }
   }
 })
